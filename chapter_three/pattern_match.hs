5715dabe45c8b3ee27153ceace78563a537d639c
kenta-s <knt01222@gmail.com>
2016-10-11 22:11:28 +0900

    Pattern match

---
+++ chapter_three/pattern_match.hs
@@ -0,0 +1,9 @@
+sayMe :: Int -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+sayMe x = "Not between 1 and 5"
+
+-- This is awesome! pattern match is awesome!!!!!!!
